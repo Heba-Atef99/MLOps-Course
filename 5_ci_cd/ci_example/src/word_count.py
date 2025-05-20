273875feed5d1126a94338aced43cc1aa447c0e8
Heba <heba.atef.muhammed@gmail.com>
2025-05-20 10:43:48 +0300

    fixing the empty body error

--- 5_ci_cd/ci_example/src/word_count.py
+++ 5_ci_cd/ci_example/src/word_count.py
@@ -3,6 +3,9 @@ def word_count_handler(event, context):
     A simple function that counts
     the number of words in a string
     """
+    if event == {}:
+        return {"error": "no body"}
+    
     msg = event["body"]
 
     n_words = len(msg.split())
